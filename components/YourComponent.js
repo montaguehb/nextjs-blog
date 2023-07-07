e99464495454e4e8e1c0f8677c7c72c1c94eac34
Historia Montague <historia.montague@outlook.com>
2023-07-07 13:15:28 -0600

    initial commit for deployment test

---
+++ components/YourComponent.js
@@ -0,0 +1,9 @@
+import Image from "next/image"
+
+const YourComponent = () => {
+  return (
+    <Image src="/images/profile.jpg" height={144} width={144} alt="Historia"/>
+  )
+}
+
+export default YourComponent
